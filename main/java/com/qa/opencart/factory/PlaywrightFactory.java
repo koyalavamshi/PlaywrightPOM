c63762ea345b368bf20bbeca66b027e18c5287e5
vamshikoyala <koyalavamshi123@gmail.com>
2026-05-20 15:08:14 +0200

    updated config.properties and PlaywrightFactory.java

--- main/java/com/qa/opencart/factory/PlaywrightFactory.java
+++ main/java/com/qa/opencart/factory/PlaywrightFactory.java
@@ -1,9 +1,7 @@
 package com.qa.opencart.factory;
 
 import com.microsoft.playwright.*;
-
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -15,38 +13,68 @@ public class PlaywrightFactory {
     Page page;
     Properties prop;
 
-    public Page initBrowser(Properties prop) {
-       String browserName = prop.getProperty("browser").trim();
-        System.out.println("browser name is " + browserName);
-        playwright = Playwright.create();
-        switch (browserName.toLowerCase()) {
-            case "chromium":
-                browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-                break;
-            case "firefox":
-                browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
-                break;
-            case "safari":
-                browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
-                break;
-            case "chrome":
-                browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false));
-                break;
-            default:
-                break;
+    private static ThreadLocal<Browser> tlBrowser = new ThreadLocal<>();
+    private static ThreadLocal<BrowserContext> tlBrowserContext = new ThreadLocal<>();
+    private static ThreadLocal<Page> tlPage = new ThreadLocal<>();
+    private static ThreadLocal<Playwright> tlPlaywright = new ThreadLocal<>();
 
-        }
-        browserContext = browser.newContext();
-        page = browserContext.newPage();
-        page.navigate(prop.getProperty("url").trim());
-        return page;
-
+    public static Playwright getPlaywright(){
+        return tlPlaywright.get();
+    }
+    public static Browser getBrowser(){
+        return tlBrowser.get();
+    }
+    public static BrowserContext getBrowserContext(){
+        return tlBrowserContext.get();
+    }
+    public static Page getPage(){
+        return tlPage.get();
     }
 
-    public Properties init_prop() throws IOException {
-        FileInputStream ip = new FileInputStream("src/test/resources/config/config.properties");
+    public Page initBrowser(Properties prop) {
+        String browserName = prop.getProperty("browser").trim();
+        System.out.println("Browser target configured: " + browserName);
+
+        // Dynamic Headless Toggle: Reads 'headless' from config.properties. Defaults to TRUE for pipeline safety.
+        boolean headlessMode = Boolean.parseBoolean(prop.getProperty("headless", "true"));
+        System.out.println("Running in Headless Mode: " + headlessMode);
+
+        tlPlaywright.set(Playwright.create());
+
+        switch (browserName.toLowerCase()) {
+            case "chromium":
+                tlBrowser.set(getPlaywright().chromium().launch(new BrowserType.LaunchOptions().setHeadless(headlessMode)));
+                break;
+            case "firefox":
+                tlBrowser.set(getPlaywright().firefox().launch(new BrowserType.LaunchOptions().setHeadless(headlessMode)));
+                break;
+            case "safari":
+                tlBrowser.set(getPlaywright().webkit().launch(new BrowserType.LaunchOptions().setHeadless(headlessMode)));
+                break;
+            case "chrome":
+                tlBrowser.set(getPlaywright().chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(headlessMode)));
+                break;
+            default:
+                throw new IllegalArgumentException("Please pass a valid browser name. Received: " + browserName);
+        }
+
+        tlBrowserContext.set(getBrowser().newContext());
+        tlPage.set(getBrowserContext().newPage());
+        getPage().navigate(prop.getProperty("url").trim());
+        return getPage();
+    }
+
+    public Properties init_prop() {
         prop = new Properties();
-        prop.load(ip);
+        // FIXED: Using a bulletproof system path resolver so it maps seamlessly on local paths or CI workspace pools
+        String configPath = System.getProperty("user.dir") + "/src/test/resources/config/config.properties";
+
+        try (FileInputStream ip = new FileInputStream(configPath)) {
+            prop.load(ip);
+        } catch (IOException e) {
+            System.err.println("CRITICAL ERROR: Unable to load config file at path: " + configPath);
+            e.printStackTrace();
+        }
         return prop;
     }
 }
--- test/java/com/qa/opencart/base/BaseTest.java
+++ test/java/com/qa/opencart/base/BaseTest.java
@@ -1,55 +1,34 @@
 package com.qa.opencart.base;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.Page;
 import com.qa.opencart.Pages.HomePage;
 import com.qa.opencart.Pages.LoginPage;
 import com.qa.opencart.factory.PlaywrightFactory;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 import java.io.IOException;
 import java.util.Properties;
 
 public class BaseTest {
     PlaywrightFactory pf;
-    Playwright playwright;
-    Browser browser;
     Page page;
-    
-    protected HomePage homePage;
-    protected LoginPage loginPage;
-    protected Properties prop;
+   protected HomePage homePage;
+   protected LoginPage loginPage;
+  protected Properties prop;
 
     @BeforeMethod
     public void setup() throws IOException {
         pf = new PlaywrightFactory();
-        prop = pf.init_prop();
-
-        // Total Override: Initialize Playwright directly here to guarantee headless execution
-        playwright = Playwright.create();
-        
-        // This line forces Chromium to launch headlessly, ignoring the factory's launch settings
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
-        
-        page = browser.newPage();
-        
-        // Pass your direct headless page straight to your Page Objects
+        prop= pf.init_prop();
+        page =pf.initBrowser(prop);
         homePage = new HomePage(page);
-    }
 
+    }
     @AfterMethod
-    public void teardown() {
-        if (page != null) {
-            page.close();
-        }
-        if (browser != null) {
-            browser.close();
-        }
-        if (playwright != null) {
-            playwright.close();
-        }
+    public void teardown(){
+        page.context().browser().close();
     }
 }
